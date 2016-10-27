625b8a86110870bd86aa9ac1d2915760c0ea5890
gaurav123337 <gaurav.123337@gmail.com>
2016-10-27 13:01:06 +0530

    first commit

---
+++ src/app/product-info/product-info.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-info',
+  templateUrl: './product-info.component.html',
+  styleUrls: ['./product-info.component.css']
+})
+export class ProductInfoComponent {
+
+  constructor() { }
+
+}
