282f0e20089d06bd879f4c10eeee6c90db4ef1c3
gaurav123337 <gaurav.123337@gmail.com>
2016-10-28 15:53:50 +0530

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductInfoComponent } from './product-info/product-info.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductInfoComponent
+    ProductInfoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
