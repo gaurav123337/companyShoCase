abfb3dbe592a61012599654550b36d0cf4a83129
gaurav123337 <gaurav.123337@gmail.com>
2016-10-28 13:06:13 +0530

    added after painting it from json file

--- src/app/product-info/product-info.component.ts
+++ src/app/product-info/product-info.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductInfo } from './product-info.modal';
+
+import { ProductListService } from './product-list.service';
+
 
 @Component({
   selector: 'product-info',
   templateUrl: './product-info.component.html',
-  styleUrls: ['./product-info.component.css']
+  styleUrls: ['./product-info.component.css'],
+  providers: [ProductListService]
 })
 export class ProductInfoComponent {
-
-  constructor() { }
-
+  productInfo : ProductInfo;
+  appInfo:  ProductInfo[];
+  constructor(private productListService: ProductListService) { }
+  
+   getAppList() {
+     let data;
+    this.productListService.getAppList().subscribe(
+      data => {
+        this.appInfo = data;
+        console.log(this.appInfo,"this.appInfo");
+      }
+    );
+  }
 }
